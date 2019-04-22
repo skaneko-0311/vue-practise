849fe340b07cb76f3f5965e0227a5ce7f1df700a
skaneko-0311 <skaneko.0311@gmail.com>
2019-04-22 23:02:56 +0900

    Make new component to shoe the header links

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ol v-for="titleID in titleIDs" :key="titleID">
+        <LinkSolver v-bind:titleID="titleID" />
+    </ol>
+  </div>
+</template>
+
+<script>
+import LinkSolver from "./LinkSolver"
+export default {
+  name: "HeaderMenu",
+  components: {
+      LinkSolver
+  },
+  data: function() {
+      return {
+          titleIDs: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      }
+  },
+  methods: {
+    makeHeaderLinks: function(menu) {
+      let out = '<a href="' + menu.link + '">' + menu.title + "</a>";
+      return out;
+    }
+  }
+};
+</script>
+
+<style scoped>
+ol {
+  display: inline-block;
+}
+</style>
---
+++ src/components/LinkSolver.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div v-for="menu in this.menus" :key="menu.id">
+      <a v-if="menu.id === titleID" :href="menu.link">{{menu.title}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LinkSolver",
+  props: ["titleID"],
+  data: function() {
+    return {
+      menus: [
+        {
+          id: 1,
+          title: "サービス一覧",
+          link: "https://www.fabercompany.co.jp/service/"
+        },
+        {
+          id: 2,
+          title: "優れたサービスの背景",
+          link: "https://www.fabercompany.co.jp/background/"
+        },
+        {
+          id: 3,
+          title: "サービス導入実績",
+          link: "https://www.fabercompany.co.jp/customer/"
+        },
+        { id: 4, title: "ミエルカブログ", link: "https://mieru-ca.com/blog/" },
+        {
+          id: 5,
+          title: "会社概要",
+          link: "https://www.fabercompany.co.jp/company/"
+        },
+        {
+          id: 6,
+          title: "お知らせ",
+          link: "https://www.fabercompany.co.jp/news/"
+        },
+        {
+          id: 7,
+          title: "採用",
+          link: "https://www.fabercompany.co.jp/recruit/"
+        },
+        { id: 8, title: "FAQ", link: "https://www.fabercompany.co.jp/faq/" },
+        {
+          id: 9,
+          title: "お問い合わせ",
+          link: "https://www.fabercompany.co.jp/contact/"
+        }
+      ]
+    };
+  },
+};
+</script>
